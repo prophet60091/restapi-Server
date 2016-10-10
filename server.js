98c8299eb017aec630a1564ac415d7ed0d545cc3
Robert <rpjackson@gmail.com>
2016-10-09 22:30:17 -0600

    updated package jsonto use server.js

--- server.js
+++ server.js
@@ -51,6 +51,8 @@ mongoose.connection.once('open', function() {
     app.use(route, controller(app, route));
     });
 
-      console.log('Listening on port 3000...');
-      app.listen(3000);
+      // console.log('Listening on port 3000...');
+      // app.listen(80);
+}).listen(process.env.PORT || 8080, function () {
+    console.log('started web process');
 });
