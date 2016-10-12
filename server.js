cdaab53ac9aca47d20df3b5a101079cbf80f8dea
Robert <rpjackson@gmail.com>
2016-10-12 00:19:33 -0600

    change ip address of serve binding

--- server.js
+++ server.js
@@ -15,32 +15,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-// app.use(bodyParser.urlencoded({'extended':'true'}));
-// app.use(bodyParser.json());
-// app.use(bodyParser.text());
-// app.use(bodyParser.json({type:'application/json'}));
 app.use(methodOverride());
 
 app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     next();
 });
 
 // CORS Support
-// app.use(function(req, res, next) {
-//     res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-//     res.header('Access-Control-Allow-Credentials', 'true');
-//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-//     res.header('Access-Control-Expose-Headers', 'Content-Length');
-//     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-//     if (req.method === 'OPTIONS') {
-//         return res.send(200);
-//     } else {
-//         return next();
-//     }
-// });
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.header('Access-Control-Expose-Headers', 'Content-Length');
+    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+    if (req.method === 'OPTIONS') {
+        return res.send(200);
+    } else {
+        return next();
+    }
+});
 
 // Connect to MongoDB
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
@@ -70,7 +68,7 @@ mongoose.connection.once('open', function() {
 
       // console.log('Listening on port 3000...');
       // app.listen(80);
-    app.listen(9000 ,function () {
+    app.listen(8080 ,function () {
         console.log('started web process');
     });
 
