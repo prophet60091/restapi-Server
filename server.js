2594a26edf40792a2ec09e7d3e51db95647e0b4d
Robert <rpjackson@gmail.com>
2016-10-12 00:01:15 -0600

    change ip address of serve binding

--- server.js
+++ server.js
@@ -21,21 +21,26 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 // app.use(bodyParser.json({type:'application/json'}));
 app.use(methodOverride());
 
-
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // CORS Support
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS') {
-        return res.send(200);
-    } else {
-        return next();
-    }
-});
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+//     res.header('Access-Control-Expose-Headers', 'Content-Length');
+//     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+//     if (req.method === 'OPTIONS') {
+//         return res.send(200);
+//     } else {
+//         return next();
+//     }
+// });
 
 // Connect to MongoDB
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
