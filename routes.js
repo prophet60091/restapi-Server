045340244872aeeeece6a114dad96ebeb8fa2a9a
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-10-21 11:34:29 -0600

    added routes for location, user and style

--- routes.js
+++ routes.js
@@ -1,3 +1,6 @@
 module.exports = {
-    'beer' :require('./controllers/BeerController')
+    'beer' :require('./controllers/BeerController'),
+    'user' :require('./controllers/UserController'),
+    'location' :require('./controllers/LocationController'),
+    'style' :require('./controllers/StyleController')
 };
