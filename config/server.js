b019330c637fbbea1465bdbdaf80123001111885
prophet60091 <jackrobe@onid.oregonstate.edu>
2016-10-24 00:53:57 -0600

    updated server port

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports= {
     'secret': 'WashUfuziAnDriveMeToFrienze',
     'database': 'mongodb://userMcUserName:YOUENJOYMYSELF@ds021166.mlab.com:21166/debeers496',
-    "port": "8080"
+    "port": "3000"
 }
