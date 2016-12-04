475186064146c8d38f2d67041e2e3f9e0424567e
prophet60091 <jackrobe@oregonstate.edu>
2016-12-03 23:41:11 -0700

    change cache control

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    res.header ('Cache-control', 'public, max-age=45000');
+    res.header ('Cache-control', 'public, max-age=0');
     if (req.method === 'OPTIONS') {
         return res.send(200);
     } else {
