eb8ba461e6a527e1c351135e2a21661c0875024e
prophet60091 <jackrobe@oregonstate.edu>
2016-12-05 11:53:02 -0700

    no cache

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    res.header ('Cache-Control', 'public, max-age=0');
+    res.header ('Cache-Control', 'no-cache');
     if (req.method === 'OPTIONS') {
         return res.send(200);
     } else {
