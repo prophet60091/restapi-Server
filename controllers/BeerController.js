b5604bbfa2fef070dee26bd97b86c3708a401ced
prophet60091 <jackrobe@oregonstate.edu>
2016-12-04 22:30:36 -0700

    trouble shooting post on Beer

--- controllers/BeerController.js
+++ controllers/BeerController.js
@@ -3,91 +3,9 @@ var passport = require('passport');
 var config = require('../config/server');
 var jwt = require('jwt-simple');
 module.exports = function(app, route) {
-    Resource(app, '', route,  app.models.beers).get({
 
-        //Check for credentials
-        before: function(req, res, next) {
-            //var result = passport.authorize('jwt', {session: false});
-            //console.log(result);
-            var token = getToken(req.headers);
-            if (token) {
-                console.log("gottoken");
-                var decoded = jwt.decode(token, config.secret);
-                app.models.users.findOne({
-                    name: decoded.name
-                }, function (err, user) {
-                    if (err) throw err;
-
-                    if (!user) {
-                        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
-                    } else {
-                        //set the user Id of the requestor
-                        userId = user._id;
-                        next();
-                    }
-                });
-            } else {
-                return res.status(403).send({success: false, msg: 'No token provided.'});
-            }
-
-        }
-    })
-    /*
-     POST
-     */
-        .post({
-            //add this post's id to the user's beer collection
-            //before: function(req, res, next){} TODO Check if location exists in system first?, then just add to the users stuff
-            userId: null,
-            before: function(req, res, next){
-                //Check for credentials
-
-                var token = getToken(req.headers);
-                if (token) {
-                    console.log("gottoken");
-                    var decoded = jwt.decode(token, config.secret);
-                    app.models.users.findOne({
-                        name: decoded.name
-                    }, function (err, user) {
-                        if (err) throw err;
-
-                        if (!user) {
-                            return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
-                        } else {
-                            //set the user Id of the requestor
-                            userId = user._id;
-                            next();
-                        }
-                    });
-                } else {
-                    return res.status(403).send({success: false, msg: 'No token provided.'});
-
-                }
-            },
-            after: function(req, res, next){
-                //console.log("response",res.resource);
-                var id = res.resource.item._id;
-
-                // /set parameters for the add
-                var condition = {"_id": userId}
-                    , update = {$addToSet:{"ubeers":{"beer":id, "loved": req.body.loved}}}    // set it to null
-                    , options = { }; // check all beer documents
-
-                var user = app.models.users.model('users');
-
-                //add it
-                user.update(condition, update, options, cb);
-
-                function cb(err, model){
-                    console.log(model);
-                }
-
-                next();
-            }
-
-        });
   // Setup the controller for REST;
- /*Resource(app, '', route,  app.models.beers).get({
+ Resource(app, '', route,  app.models.beers).get({
      userId: null,
      //Check for credentials
      before: function(req, res, next) {
@@ -169,9 +87,9 @@ module.exports = function(app, route) {
 
          next();
      }
-     })/!*
+     })/*
   DELETE
-  *!/
+  */
      .delete({
          userId: null,
          before: function (req, res, next) {
@@ -223,8 +141,6 @@ module.exports = function(app, route) {
              next();
          }
      }).index({
-
-
      userId: null,
      //Check for credentials
      before: function(req, res, next) {
@@ -281,7 +197,7 @@ module.exports = function(app, route) {
          }
      }
 
-    });*/
+    });
     getToken = function (headers) {
         if (headers && headers.authorization) {
             var parted = headers.authorization.split(' ');
