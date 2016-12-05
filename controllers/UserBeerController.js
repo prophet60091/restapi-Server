01735771ee19b1fe80a2ffb66cd9cfc94e2ccd96
prophet60091 <jackrobe@oregonstate.edu>
2016-12-04 22:24:50 -0700

    add post to mybeer

--- controllers/UserBeerController.js
+++ controllers/UserBeerController.js
@@ -163,32 +163,7 @@ module.exports = function(app, route) {
                         //set the user Id of the requestor
                         userId = user._id;
                         //console.log(_.map(user.ubeer, 'beer'));
-
-                        /*//add his beer
-                        console.log('-------add beer in user RECORD------');
-
-                        // // get the style id
-                        //var beerid = res.resource.item._id;
-                        console.log("this should be added:", beerid);
-                        console.log("To this user:", userId);
-
-                        // /set parameters for the search
-                        var condition = {_id: userId},
-                            update = {$addToSet: {ubeers: {beer: beerid}}},
-                            options = {};
-
-                        var userModel = app.models.users.model('users');
-
-                        //remove it
-                        userModel.update(condition, update, options, function (err, numAffected) {
-                            console.log(numAffected.ok);
-                            console.log(numAffected);
-                            if (numAffected.nModified == 1) {
-                                return res.status(200).send();
-                            }
-                        });*/
-
-                        console.log('----------------------------');
+                        next();
                     }
                 });
             } else {
@@ -198,6 +173,59 @@ module.exports = function(app, route) {
             next();
         },
             //NOW ADD the beer to the users stash
+        after: function(req, res, next){
+            var id = res.resource.item._id;
+            //console.log("idtopush", id);
+            //console.log("userId to add it to", userId);
+
+            // /set parameters for the add
+            var condition = {"_id": userId}
+                , update = {$addToSet:{"ubeers":{"beer":id, "loved": req.body.loved}}}    // set it to null
+                , options = { }; // check all beer documents
+
+            var user = app.models.users.model('users');
+
+            //add it
+            user.update(condition, update, options, cb);
+
+            function cb(err, model){
+                console.log(model);
+            }
+
+            next();
+        }
+
+    })
+        .post({
+        before: function (req, res, next) {
+            console.log(req.params);
+            var beerid = req.params.mybeersId;
+            var token = getToken(req.headers);
+            if (token) {
+                console.log("gottoken");
+                var decoded = jwt.decode(token, config.secret);
+                app.models.users.findOne({
+                    name: decoded.name
+                }, function (err, user) {
+                    if (err) throw err;
+
+                    if (!user) {
+                        return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+                    } else {
+                        //set the user Id of the requestor
+                        userId = user._id;
+                        //console.log(_.map(user.ubeer, 'beer'));
+
+                        next();
+                    }
+                });
+            } else {
+                return res.status(403).send({success: false, msg: 'No token provided.'});
+
+            }
+            next();
+        },
+        //NOW ADD the beer to the users stash
         after: function(req, res, next){
             var id = res.resource.item._id;
             //console.log("idtopush", id);
